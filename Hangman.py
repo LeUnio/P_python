5236c8e6a09b8044b4c015cae96e8fb7190d1a6f
LeUnio <78481749+LeUnio@users.noreply.github.com>
2021-10-08 09:29:45 +0200

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,14 @@
+import random
+
+alfabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "æ", "ø", "å"]
+
+
+def random_valg (x):
+    ord = random.sample(alfabet, x)
+
+
+def ord_valg(x):
+    f = open("")
+
+
+
